4922c6600848ca3930560350b746d2cfdb7cd9fd
Asif Hussain <asifnawada786@gmail.com>
2026-03-11 11:45:59 +0530

    Add student details input and output program

---
+++ Experiment-01_CPP_IO/student_details.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name,roll;
+    int age;
+    
+    cout<<"Enter student name, roll number and age: ";
+    cin>>name>>roll>>age;
+    
+    cout<<"Student Details:\n";
+    cout<<"Name: "<<name<<"\n";
+    cout<<"Roll Number: "<<roll<<"\n";
+    cout<<"Age: "<<age<<"\n";
+
+    return 0;
+}
