ca1abc0e4cbe2dee86f480b9b8d76185b498c6d1
asifdevlab <asifnawada786@gmail.com>
2026-03-18 11:08:09 +0530

    Practiced some basic in-built functions of the string

---
+++ CPP-Basics-Drilling/strings/string_functions.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    /*string str = "Programming";
+    cout<<str.length()<<endl;
+    cout<<sizeof(str)<<endl;
+    cout<<str.size()<<endl;*/
+
+    /*string str1 = "Hello ";
+    string str2 = "World";
+
+    str1.append(str2);
+    cout<< str1;
+
+    cout<<sizeof(str1)<<endl;
+    cout<<str1.size()<<endl;*/
+
+    /*string str = "Helo";
+
+    str.insert(2,"l");
+
+    cout << str;*/
+
+    /*string str = "Programming";
+
+    str.erase(3,4);
+
+    cout << str;*/
+
+    /*string str = "Programming";
+    cout << str.substr(3,4);*/
+
+    /*string str = "Hello World";
+
+    cout << str.find("World");*/
+
+    string str = "Hello World";
+    str.replace(6,5,"C++");
+    cout<<str;
+
+
+return 0;
+    
+}
